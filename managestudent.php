860ea822174b49dc9a8fe9c6e2e21d07e2fe2520
najitha-123-github <najithanazar786@gmail.com>
2024-09-02 19:05:51 +0530

    Create managestudent.php

---
+++ managestudent.php
@@ -0,0 +1,68 @@
+<html>
+    <head>
+        <title>Manage Users</title>
+        <link rel="stylesheet" href="./managestudent.css">
+    </head>
+    <body>
+        <nav class="ViewCandidatesNav">
+            <h1 class="ViewCandidatesNavHeading">View Candidates</h1>
+            <div class="ViewCandidatesNavContainer">
+            <a href="admin.html">Home</a>
+            </div>
+        </nav>
+        <div class="ViewCandidatesBodyContainer">
+            <h1>Candidates</h1>
+            <?php
+    $conn = mysqli_connect("localhost", "root", "", "online_library_management_system");
+    if(!$conn){
+        echo "Database not connected";
+    }
+    $sql = "SELECT * FROM `users`";
+    $data=mysqli_query($conn,$sql);
+    if(mysqli_num_rows($data)>0){  
+        echo "<table border=1 >";
+        echo "<tr>";
+        echo "<th>Name</th>";
+        echo "<th>Email</th>";
+        echo "<th>Phone Number</th>";
+        echo "<th>Username</th>";
+        echo "<th>User ID</th>";
+        echo "</tr>";
+        while($row=mysqli_fetch_assoc($data)){
+            $email = $row['email'];
+            echo "<tr>";
+            echo "<td>".$row['name']."</td>";
+            echo "<td>".$row['email']."</td>";
+            echo "<td>".$row['phonenumber']."</td>";
+            echo "<td>".$row['username']."</td>";
+            echo "<td>".$row['usid']."</td>";
+            echo "<td>
+                    <form method='POST'>
+                        <button value='$email' name='userdel' type='submit'>Delete</button>
+                    </form>
+                </td>";
+            echo "<td><button>Edit</button></td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    }
+?>
+        </div>
+    </body>
+</html>
+<?php
+$conn = mysqli_connect("localhost", "root", "", "online_library_management_system");
+if(!$conn){
+    echo "Database not connected";
+}
+if(isset($_POST['userdel'])){
+    $email = $_POST['userdel'];
+    if(!empty($_POST['userdel'])){
+        $sql = "DELETE FROM `users` WHERE `email`='$email'";
+        $data = mysqli_query($conn, $sql);
+        $sql1 = "DELETE FROM `login` WHERE `email`='$email'";
+        $data1 = mysqli_query($conn, $sql1);
+        echo "<script>window.location.replace('./managestudent.php');</script>";
+    }
+}
+?>
